27e0f4c4b999fa52afb4e262c5b8c48cf9cb3819
D13GO91 <diego.amador@outlook.com>
2021-11-01 14:18:21 -0300

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,12 @@
-import 'assets/css/styles.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./assets/css/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
